32679049d98ccb0c46963d91619ebc35006bade2
Daniel Ashcroft <dashcroft4@gmail.com>
2019-08-02 16:03:11 +0100

    improved profil page.

--- src/main/java/ecco/onestopshop/controllers/LoginController.java
+++ src/main/java/ecco/onestopshop/controllers/LoginController.java
@@ -9,6 +9,7 @@ import ecco.onestopshop.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
 
 @Controller
 public class LoginController {
@@ -38,10 +39,18 @@ public class LoginController {
     }
 
     @RequestMapping("/profil")
-    public String profil(){
+    public String profil(Model model){
         if(userCanEnter)
             return "profil";
         else
             return "login";
+    }
+
+    @RequestMapping(value = "/profilData", method = RequestMethod.POST)
+    @ResponseBody
+    public void updateUserData(@RequestBody String[] profilData){
+
+        for(int i = 0; i < profilData.length; i++)
+            System.out.println(userLoggedIn + " is from "+profilData[i]);
     }
 }
