3dd8eacb2a1fa27b039836aa0bc0995bd49cc63c
Daniel Ashcroft <dashcroft4@gmail.com>
2019-10-22 10:21:17 +0100

    updated neo4jrepository

--- src/main/java/ecco/onestopshop/models/repositories/Neo4jUserRepository.java
+++ src/main/java/ecco/onestopshop/models/repositories/Neo4jUserRepository.java
@@ -27,13 +27,15 @@ public interface Neo4jUserRepository extends Neo4jRepository<User, Long> {
     @Query("MATCH (u:User{email:{0}, password:{1}}) RETURN u Limit 1")
     User getByLogin(String email, String password);
 
+//    @Query("MATCH (u:User{username:{0}}) MERGE (u)-[:LIVES_IN]->(newL:Location{username:{1}}) ")
     @Query("MATCH (newL:Location{username:{1}}), (u:User{username:{0}}) MERGE (u)-[:LIVES_IN]->(newL) ")
     void setUserLocation(String username, String locationName);
 
     @Query("MATCH (u:User{username:{0}}), (u)-[r:LIVES_IN]->(:Location) DELETE r")
     void removeUserLocation(String username);
 
-    @Query("MATCH (u:User{username:{1}}) MERGE (u)-[:USES]->(r:RenewableTechnology{name:{0}})")
+//    @Query("MATCH (u:User{username:{1}}) MERGE (u)-[:USES]->(r:RenewableTechnology{name:{0}})")
+    @Query("MATCH (u:User{username:{1}}),(r:RenewableTechnology{name:{0}})  MERGE (u)-[:USES]->(r)")
     void setUserTechnology(String renewableTechName, String userLoggedInName);
 
     @Query("MATCH (w:User{username:{0}}), (w)-[r:USES]->(:RenewableTechnology) DELETE r")
